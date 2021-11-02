f3314be101670e4da845442d5d7e3c0d77fc1b62
reinhardwafula <rehynsanders@gmail.com>
2021-11-02 15:20:56 +0300

    Map Activity

---
+++ app/src/main/java/com/example/maps/MapsActivity.kt
@@ -0,0 +1,52 @@
+package com.example.maps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.example.maps.databinding.ActivityMapsBinding
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val nai = LatLng(-1.268665592, 36.805996776)
+        mMap.addMarker(MarkerOptions().position(nai).title("Marker in Westlands"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(nai))
+
+        val eldoret = LatLng(0.52036, 35.26993)
+        mMap.addMarker(MarkerOptions().position(eldoret).title("Marker in Eldoret"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(eldoret))
+    }
+}
